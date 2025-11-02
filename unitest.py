f04fabf1579fdd9173496d247874a047a3195d99
ilaishimoni <74491683+ilaishimoni@users.noreply.github.com>
2025-11-02 15:42:57 +0200

    redo comment error

--- unitest.py
+++ unitest.py
@@ -14,22 +14,22 @@ def test_get_user_email(user_manager):
     user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
     assert user_manager.get_user_email("Ilai") == "ilaishimoni@gmail.com"
 
-# def test_add_existing_user(user_manager):
-#     user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
-#     with pytest.raises(ValueError, match="User already exists"):
-#         user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
-#
-# def test_get_non_existing_user(user_manager):
-#     with pytest.raises(ValueError, match="User does not exist"):
-#         user_manager.get_user_email("ilaishimoni@gmail.com")
-#
-# def test_get_multiple_users(user_manager):
-#     user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
-#     user_manager.add_user("Dan", "fatdan@gmail.com")
-#
-#     expected_users = {
-#         "Ilai": "ilaishimoni@gmail.com",
-#         "Dan": "fatdan@gmail.com",
-#     }
-#
-#     assert user_manager.get_all_users() == expected_users
+def test_add_existing_user(user_manager):
+    user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
+    with pytest.raises(ValueError, match="User already exists"):
+        user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
+
+def test_get_non_existing_user(user_manager):
+    with pytest.raises(ValueError, match="User does not exist"):
+        user_manager.get_user_email("ilaishimoni@gmail.com")
+
+def test_get_multiple_users(user_manager):
+    user_manager.add_user("Ilai", "ilaishimoni@gmail.com")
+    user_manager.add_user("Dan", "fatdan@gmail.com")
+
+    expected_users = {
+        "Ilai": "ilaishimoni@gmail.com",
+        "Dan": "fatdan@gmail.com",
+    }
+
+    assert user_manager.get_all_users() == expected_users
